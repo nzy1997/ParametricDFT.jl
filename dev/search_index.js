80fd9b6871aa270b71a9c32e66e259541d388782
Documenter.jl <documenter@juliadocs.github.io>
2025-10-08 01:30:08 +0000

    build based on e38b8d5

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#QDFT","page":"Home","title":"QDFT","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for QDFT.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
