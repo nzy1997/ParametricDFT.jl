1d7eeb58ecaa7ddd11f19a926fc8573367353259
Documenter.jl <documenter@juliadocs.github.io>
2025-10-13 09:28:18 +0000

    build based on b7ce63e

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#QDFT","page":"Home","title":"QDFT","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for QDFT.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+[{"location":"#ParametricDFT","page":"Home","title":"ParametricDFT","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ParametricDFT.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
 }
